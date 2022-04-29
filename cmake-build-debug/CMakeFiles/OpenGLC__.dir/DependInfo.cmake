131efe7f9aa362596a78b6f812d3c3eb6436da7d
Timicasto Index <2505084982@qq.com>
2022-04-29 16:46:09 +0800

    adding assimp dep

---
+++ cmake-build-debug/CMakeFiles/OpenGLC__.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/gezhiqiang/TheNullPointerException/TheNullPointerException/glad.c" "/home/gezhiqiang/TheNullPointerException/TheNullPointerException/cmake-build-debug/CMakeFiles/OpenGLC__.dir/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../glm"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gezhiqiang/TheNullPointerException/TheNullPointerException/Profile.cpp" "/home/gezhiqiang/TheNullPointerException/TheNullPointerException/cmake-build-debug/CMakeFiles/OpenGLC__.dir/Profile.cpp.o"
+  "/home/gezhiqiang/TheNullPointerException/TheNullPointerException/Shader.cpp" "/home/gezhiqiang/TheNullPointerException/TheNullPointerException/cmake-build-debug/CMakeFiles/OpenGLC__.dir/Shader.cpp.o"
+  "/home/gezhiqiang/TheNullPointerException/TheNullPointerException/font.cpp" "/home/gezhiqiang/TheNullPointerException/TheNullPointerException/cmake-build-debug/CMakeFiles/OpenGLC__.dir/font.cpp.o"
+  "/home/gezhiqiang/TheNullPointerException/TheNullPointerException/main.cpp" "/home/gezhiqiang/TheNullPointerException/TheNullPointerException/cmake-build-debug/CMakeFiles/OpenGLC__.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../glm"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/OpenGLC__.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLC__.dir/Profile.cpp.o"
+  "CMakeFiles/OpenGLC__.dir/Shader.cpp.o"
+  "CMakeFiles/OpenGLC__.dir/font.cpp.o"
+  "CMakeFiles/OpenGLC__.dir/glad.c.o"
+  "CMakeFiles/OpenGLC__.dir/main.cpp.o"
+  "OpenGLC__"
+  "OpenGLC__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenGLC__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
